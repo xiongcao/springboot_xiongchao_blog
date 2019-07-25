4bfc7c204db9025263793c20b4baaa01142262ec
xiongchao <1359209000@qq.com>
2019-07-25 17:58:42 +0800

    文章列表页添加标签、类型字段

--- src/main/java/com/xiongchao/blog/DTO/EssayDTO.java
+++ src/main/java/com/xiongchao/blog/DTO/EssayDTO.java
@@ -1,7 +1,6 @@
 package com.xiongchao.blog.DTO;
 
 import com.xiongchao.blog.bean.Category;
-import com.xiongchao.blog.bean.Comment;
 import com.xiongchao.blog.bean.Essay;
 import com.xiongchao.blog.bean.Tag;
 
@@ -12,8 +11,6 @@ public class EssayDTO extends Essay {
     private List<Tag> tags;
 
     private List<Category> categories;
-
-    private List<Comment> comments;
 
     public List<Tag> getTags() {
         return tags;
@@ -29,13 +26,5 @@ public class EssayDTO extends Essay {
 
     public void setCategories(List<Category> categories) {
         this.categories = categories;
-    }
-
-    public List<Comment> getComments() {
-        return comments;
-    }
-
-    public void setComments(List<Comment> comments) {
-        this.comments = comments;
     }
 }
--- src/main/java/com/xiongchao/blog/dao/CategoryRepository.java
+++ src/main/java/com/xiongchao/blog/dao/CategoryRepository.java
@@ -2,6 +2,7 @@ package com.xiongchao.blog.dao;
 
 
 import com.xiongchao.blog.bean.Category;
+import com.xiongchao.blog.bean.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -35,4 +36,12 @@ public interface CategoryRepository extends JpaRepository<Category, Integer> {
      */
     @Query(value = "select * from category c WHERE c.user_id = ? and c.status <> -1 ORDER BY c.rank DESC", nativeQuery = true)
     List<Category> findAllByUserIdAndStatus(Integer userId);
+
+    /**
+     * 根据文章ID查询所有类型
+     * @param essayId
+     * @return
+     */
+    @Query(value = "SELECT c.* FROM category c LEFT JOIN essay_category_mapping ec ON c.id = ec.category_id WHERE ec.essay_id = ? AND c.status <> 0", nativeQuery = true)
+    List<Category> findListByEssayId(Integer essayId);
 }
